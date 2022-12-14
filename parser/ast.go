3f3bc0970ed3c67bef0bb326c0ecd6ba20735949
Karina <abraham.karina.dev@gmail.com>
2022-12-14 12:32:13 -0300

    wip

--- parser/ast.go
+++ parser/ast.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"github.com/NoetherianRing/c8-compiler/token"
 )
 
@@ -33,19 +32,3 @@ func (node *Node) AddChild(child *Node){
 	node.children = append(node.children, child)
 }
 
-func (tree *SyntaxTree) debug(){
-	debug(tree, 0)
-}
-func debug(tree *SyntaxTree, nesting int){
-
-	for _, child := range tree.head.children{
-
-		fmt.Printf("[%d]", nesting)
-		fmt.Printf(" %s\n", child.value.Literal)
-
-
-		auxTree := NewSyntaxTree(child)
-		debug(auxTree, nesting + 1)
-
-	}
-}
