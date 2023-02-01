34d1217697b171cf695deed1ea29653fcf4efc6d
Karina <abraham.karina.dev@gmail.com>
2023-02-01 15:19:52 -0300

    wip

--- symboltable/symboltable.go
+++ symboltable/symboltable.go
@@ -13,7 +13,7 @@ const(
 	KindVoid
 	KindBool
 )
-const UnknownLength = -1
+
 type Scope struct{
 	SubScopes []*Scope
 	NumberOfSubScope int
@@ -62,11 +62,10 @@ func (array Array)Compare(datatype interface{})bool{
 		return false
 	}
 
-	if toCompare.Length != UnknownLength && array.Length !=UnknownLength{
-		if toCompare.Length != array.Length{
+	if toCompare.Length != array.Length{
 			return false
 		}
-	}
+
 
 	if reflect.TypeOf(array.Of) != reflect.TypeOf(toCompare.Of){
 		return false
@@ -105,11 +104,7 @@ func Fmt(datatype interface{}) string{
 		return "*"+ Fmt(datatype.(Pointer).PointsTo)
 	case Array:
 		array := datatype.(Array)
-		if array.Length != UnknownLength{
-			return "["+strconv.Itoa(array.Length)+"]"+Fmt(array.Of)
-		}else{
-			return "[byte]"+Fmt(array.Of)
-		}
+		return "["+strconv.Itoa(array.Length)+"]"+Fmt(array.Of)
 	case Simple:
 		simpleDataType :=  datatype.(Simple)
 		if simpleDataType.Kind == KindByte{
